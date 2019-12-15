e8afd2e8e8a61a2b34a98141398a4924a7bcf3a4
Andrew Whelan <whelandrew@gmail.com>
2019-12-15 14:16:51 -0800

    first

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  devtool: 'inline-source-map',
+  entry: './src/index.js',
+  output: {
+    path: __dirname + 'build',//'/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: "./build",//'./dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve('./index.html'),
+    }),
+  ],
+  module: {
+  rules: [{
+    test: /\.(js|jsx)$/,
+    exclude: /node_modules/,
+    use: ['babel-loader', 'eslint-loader']
+  },
+  {
+    test: /\.less$/,
+    use: [
+      'style-loader',
+      'css-loader',
+      'less-loader',
+    ],
+  },
+ ]
+}
+};
